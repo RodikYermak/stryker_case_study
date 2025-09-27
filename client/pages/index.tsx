e34aeb65b9c759b755e48e82507aa2a0e1ddae1a
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-09-27 14:01:06 -0700

    connected frontend

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,16 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import UserInterface from '@/components/UserInterface';
 
-function Index() {
-    const [message, setMessage] = useState('Loading');
-    useEffect(() => {
-        fetch('http://localhost:8080/')
-            .then((response) => response.json())
-            .then((data) => {
-                // console.log(data);
-                setMessage(data.message);
-            });
-    }, []);
-    return <div>{message}</div>;
-}
+const Home: React.FC = () => {
+    return (
+        <div>
+            <UserInterface backendName="flask" />
+        </div>
+    );
+};
 
-export default Index;
+export default Home;
