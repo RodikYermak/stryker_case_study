0aabbbfabd134ba91ab52960e8e9a6b76d64c980
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-09-26 20:31:07 -0700

    commit

--- server/server.py
+++ server/server.py
@@ -1,13 +1,14 @@
 from flask import Flask, jsonify
-
+from flask_cors import CORS
 # app instance
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/api/home", methods=['GET'])
+@app.route("/", methods=['GET'])
 def return_home():
     return jsonify({
-        'message': "Hello world!7"
+        'message': "Hello world!!"
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
