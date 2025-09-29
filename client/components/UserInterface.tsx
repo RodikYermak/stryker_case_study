3499243c8d8c0435bf5c8c10f807d7aa140c3ccf
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-09-28 18:37:49 -0700

    commit

--- client/components/UserInterface.tsx
+++ client/components/UserInterface.tsx
@@ -89,81 +89,82 @@ const UserInterface: React.FC<UserInterfaceProps> = ({ backendName }) => {
     };
 
     return (
-        <div
-            className={`user-interface ${bgColor} ${backendName} w-full max-w-md p-4 my-4 rounded shadow`}>
-            <img
-                src={`/${backendName}logo.svg`}
-                alt={`${backendName} Logo`}
-                className="w-20 h-20 mb-6 mx-auto"
-            />
-            <h2 className="text-xl font-bold text-center text-white mb-6">{`${
-                backendName.charAt(0).toUpperCase() + backendName.slice(1)
-            } Backend`}</h2>
+        // <div
+        //     className={`user-interface ${bgColor} ${backendName} w-full max-w-md p-4 my-4 rounded shadow`}>
+        //     <img
+        //         src={`/${backendName}logo.svg`}
+        //         alt={`${backendName} Logo`}
+        //         className="w-20 h-20 mb-6 mx-auto"
+        //     />
+        //     <h2 className="text-xl font-bold text-center text-white mb-6">{`${
+        //         backendName.charAt(0).toUpperCase() + backendName.slice(1)
+        //     } Backend`}</h2>
 
-            {/* Create user */}
-            <form onSubmit={createUser} className="mb-6 p-4 bg-blue-100 rounded shadow">
-                <input
-                    placeholder="Name"
-                    value={newUser.name}
-                    onChange={(e) => setNewUser({ ...newUser, name: e.target.value })}
-                    className="mb-2 w-full p-2 border border-gray-300 rounded"
-                />
-                <input
-                    placeholder="Email"
-                    value={newUser.email}
-                    onChange={(e) => setNewUser({ ...newUser, email: e.target.value })}
-                    className="mb-2 w-full p-2 border border-gray-300 rounded"
-                />
-                <button
-                    type="submit"
-                    className="w-full p-2 text-white bg-blue-500 rounded hover:bg-blue-600">
-                    Add User
-                </button>
-            </form>
+        //     {/* Create user */}
+        //     <form onSubmit={createUser} className="mb-6 p-4 bg-blue-100 rounded shadow">
+        //         <input
+        //             placeholder="Name"
+        //             value={newUser.name}
+        //             onChange={(e) => setNewUser({ ...newUser, name: e.target.value })}
+        //             className="mb-2 w-full p-2 border border-gray-300 rounded"
+        //         />
+        //         <input
+        //             placeholder="Email"
+        //             value={newUser.email}
+        //             onChange={(e) => setNewUser({ ...newUser, email: e.target.value })}
+        //             className="mb-2 w-full p-2 border border-gray-300 rounded"
+        //         />
+        //         <button
+        //             type="submit"
+        //             className="w-full p-2 text-white bg-blue-500 rounded hover:bg-blue-600">
+        //             Add User
+        //         </button>
+        //     </form>
 
-            {/* Update user */}
-            <form onSubmit={handleUpdateUser} className="mb-6 p-4 bg-blue-100 rounded shadow">
-                <input
-                    placeholder="User Id"
-                    value={updateUser.id}
-                    onChange={(e) => setUpdateUser({ ...updateUser, id: e.target.value })}
-                    className="mb-2 w-full p-2 border border-gray-300 rounded"
-                />
-                <input
-                    placeholder="Name"
-                    value={updateUser.name}
-                    onChange={(e) => setUpdateUser({ ...updateUser, name: e.target.value })}
-                    className="mb-2 w-full p-2 border border-gray-300 rounded"
-                />
-                <input
-                    placeholder="Email"
-                    value={updateUser.email}
-                    onChange={(e) => setUpdateUser({ ...updateUser, email: e.target.value })}
-                    className="mb-2 w-full p-2 border border-gray-300 rounded"
-                />
-                <button
-                    type="submit"
-                    className="w-full p-2 text-white bg-green-500 rounded hover:bg-green-600">
-                    Update User
-                </button>
-            </form>
+        //     {/* Update user */}
+        //     <form onSubmit={handleUpdateUser} className="mb-6 p-4 bg-blue-100 rounded shadow">
+        //         <input
+        //             placeholder="User Id"
+        //             value={updateUser.id}
+        //             onChange={(e) => setUpdateUser({ ...updateUser, id: e.target.value })}
+        //             className="mb-2 w-full p-2 border border-gray-300 rounded"
+        //         />
+        //         <input
+        //             placeholder="Name"
+        //             value={updateUser.name}
+        //             onChange={(e) => setUpdateUser({ ...updateUser, name: e.target.value })}
+        //             className="mb-2 w-full p-2 border border-gray-300 rounded"
+        //         />
+        //         <input
+        //             placeholder="Email"
+        //             value={updateUser.email}
+        //             onChange={(e) => setUpdateUser({ ...updateUser, email: e.target.value })}
+        //             className="mb-2 w-full p-2 border border-gray-300 rounded"
+        //         />
+        //         <button
+        //             type="submit"
+        //             className="w-full p-2 text-white bg-green-500 rounded hover:bg-green-600">
+        //             Update User
+        //         </button>
+        //     </form>
 
-            {/* Display users */}
-            <div className="space-y-4">
-                {users.map((user) => (
-                    <div
-                        key={user.id}
-                        className="flex items-center justify-between bg-white p-4 rounded-lg shadow">
-                        <CardComponent card={user} />
-                        <button
-                            onClick={() => deleteUser(user.id)}
-                            className={`${btnColor} text-white py-2 px-4 rounded`}>
-                            Delete User
-                        </button>
-                    </div>
-                ))}
-            </div>
-        </div>
+        //     {/* Display users */}
+        //     <div className="space-y-4">
+        //         {users.map((user) => (
+        //             <div
+        //                 key={user.id}
+        //                 className="flex items-center justify-between bg-white p-4 rounded-lg shadow">
+        //                 <CardComponent card={user} />
+        //                 <button
+        //                     onClick={() => deleteUser(user.id)}
+        //                     className={`${btnColor} text-white py-2 px-4 rounded`}>
+        //                     Delete User
+        //                 </button>
+        //             </div>
+        //         ))}
+        //     </div>
+        // </div>
+        <h1>Client</h1>
     );
 };
 
