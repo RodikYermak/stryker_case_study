e5fd3c85e45bf15a568839db448ac2075c514597
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-09-28 19:14:27 -0700

    sharper logo and icon

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="app-header">
       <div className="container header-wrap">
         <div className="header-left">
-          <Image className="logo" src="/logo.png" alt="Stryker logo" width={28} height={28} />
+          <Image className="logo" src="/logo.svg" alt="Stryker logo" width={28} height={28} />
           <span className="v-divider" aria-hidden="true" />
           <span className="app-title">AI DATA EXTRACTION TOOL</span>
         </div>
@@ -16,7 +16,7 @@ export default function Header() {
             <span className="credits-label">Credits</span>
           </div>
           <button className="user-btn" aria-label="Account & settings">
-            <Image src="/icons/user.png" alt="" width={18} height={18} />
+            <Image src="/user.png" alt="" width={18} height={18} />
           </button>
         </div>
       </div>
